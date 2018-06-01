bc9faf7c06b7c316f0bbf4e2a9a34943a0defd11
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-01 20:57:38 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿// Novel Game Engine LINKS for C++
+// Coding by S.H.(GAMELINKS)
+
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+	if (DxLib_Init() == -1)	// ＤＸライブラリ初期化処理
+	{
+		return -1;				// エラーが起きたら直ちに終了
+	}
+
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;					// ソフトの終了
+}
