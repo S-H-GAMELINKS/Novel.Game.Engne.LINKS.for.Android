77849dc6b78fad696fe23882e6da4c95afb2b958
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-02 00:16:41 +0900

    draw GameTitle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 // Coding by S.H.(GAMELINKS)
 
 #include "DxLib.h"
+#include <string>
+#include <array>
+
+//タイトル画面
+void GameTitle() {
+
+	int Color = DxLib::GetColor(255, 255, 255);
+
+	std::array<std::string, 4> TitleItem = { "Start", "Load", "Config", "Exit" };
+
+	while (true) {
+
+		for (int i = 0; i < 4; i++)
+			DxLib::DrawString(250, 240 - 32 * i, TitleItem[i].c_str(), Color);
+
+		if (DxLib::WaitKey())
+			break;
+	}
+}
 
 int android_main(void) {
 
@@ -10,6 +29,7 @@ int android_main(void) {
 		return -1;				// エラーが起きたら直ちに終了
 	}
 
+	GameTitle();
 
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
 
